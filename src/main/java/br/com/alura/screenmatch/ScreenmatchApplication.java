4387b089e8363aaae953304c88e81e15a23cbdc0
Cauan Henrique <cauanhenrique56@gmail.com>
2025-05-25 18:14:20 -0300

    Primeiro Commit

---
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,32 @@
+package br.com.alura.screenmatch;
+
+import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.service.ConsumoAPI;
+import br.com.alura.screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Scanner scanner = new Scanner(System.in);
+
+		var consumoAPI = new ConsumoAPI();
+		var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=dexter&apikey=bf85c372+");
+		System.out.println(json);
+
+		ConverteDados conversor = new ConverteDados();
+
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+}
